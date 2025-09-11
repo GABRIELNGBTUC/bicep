1643737423d80921144d9816944c145b8ae96ec8
Gabriel Ngandu-Biseba <gabriel.ngandu-biseba@tucrail.be>
2025-09-11 09:49:15 +0200

    Add new attributes to configure the properties of a bicep string in extensions

---
+++ src/Bicep.Local.Extension/Types/Attributes/StringPropertyAttribute.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Bicep.Local.Extension.Types.Attributes;
+
+[AttributeUsage(AttributeTargets.Property)]
+public class BicepStringMaxLengthAttribute : Attribute
+{
+    public int MaxLength { get; }
+
+    public BicepStringMaxLengthAttribute(int maxLength)
+    {
+        MaxLength = maxLength;
+    }
+}
+
+[AttributeUsage(AttributeTargets.Property)]
+public class BicepStringMinLengthAttribute : Attribute
+{
+    public int MinLength { get; }
+
+    public BicepStringMinLengthAttribute(int minLength)
+    {
+        MinLength = minLength;
+    }
+}
+
+[AttributeUsage(AttributeTargets.Property)]
+public class BicepStringPatternAttribute : Attribute
+{
+    public string Regex { get; }
+
+    public BicepStringPatternAttribute(string regex)
+    {
+        Regex = regex;
+    }
+}
