bd98458918ce4457c9606e1202835a51d886519f
Gabriel Ngandu-Biseba <gabriel.ngandu-biseba@tucrail.be>
2025-09-11 10:18:20 +0200

    Add an "IsNullable" property to the "TypeProperty" attribute in local extensions

--- src/Bicep.Local.Extension/Types/Attributes/TypePropertyAttribute.cs
+++ src/Bicep.Local.Extension/Types/Attributes/TypePropertyAttribute.cs
@@ -21,14 +21,17 @@ public class TypePropertyAttribute : Attribute
     /// <param name="description">A human-readable description of the property, or <c>null</c> if not specified.</param>
     /// <param name="flags">Flags that describe the property's characteristics (e.g., required, read-only).</param>
     /// <param name="isSecure">Indicates whether the property contains sensitive information and should be treated as secure.</param>
+    /// <param name="isNullable">Indicates whether the property should be treated as nullable by bicep</param>
     public TypePropertyAttribute(
         string? description,
         ObjectTypePropertyFlags flags = ObjectTypePropertyFlags.None,
-        bool isSecure = false)
+        bool isSecure = false,
+        bool isNullable = false)
     {
         Description = description ?? string.Empty;
         Flags = flags;
         IsSecure = isSecure;
+        IsNullable = isNullable;
     }
 
     /// <summary>
@@ -45,4 +48,9 @@ public class TypePropertyAttribute : Attribute
     /// Gets a value indicating whether the property contains sensitive information.
     /// </summary>
     public bool IsSecure { get; }
+
+    /// <summary>
+    /// Gets a value indicating whether the property should be marked as nullable by bicep
+    /// </summary>
+    public bool IsNullable { get; }
 }
